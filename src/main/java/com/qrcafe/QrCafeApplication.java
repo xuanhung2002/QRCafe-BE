98c94a1dd290a9e497e15e8c55eed6c2d22fa11a
xuanhung2002 <xuanhung09052002@gmail.com>
2023-09-29 14:41:59 +0700

    init project

---
+++ src/main/java/com/qrcafe/QrCafeApplication.java
@@ -0,0 +1,13 @@
+package com.qrcafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrCafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrCafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qrcafe/QrCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qrcafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
