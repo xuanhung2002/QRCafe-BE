2aa8b5dc60cb721dffd5e718efe433db7695c0cc
cmvn-thinhtran <thinh.tran@classmethod.vn>
2023-12-05 14:27:25 +0700

    update table entity and add call staff api

--- src/main/java/com/qrcafe/controller/TableController.java
+++ src/main/java/com/qrcafe/controller/TableController.java
@@ -1,14 +1,20 @@
 package com.qrcafe.controller;
 
-import com.qrcafe.entity.Table;
-import com.qrcafe.enums.TableStatus;
-import com.qrcafe.service.TableService;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.UUID;
+import com.qrcafe.entity.Table;
+import com.qrcafe.enums.TableStatus;
+import com.qrcafe.service.TableService;
 
 @RestController
 @RequestMapping("/api/table")
@@ -34,7 +40,7 @@ public class TableController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> getProductById(@PathVariable UUID id) {
+    public ResponseEntity<?> getTableById(@PathVariable UUID id) {
         Object table = tableService.getTableById(id);
         if (table == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No item have this ID");
