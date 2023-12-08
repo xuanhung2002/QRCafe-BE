0b2c98e99366390a022e92b789c103199842f6c5
cmvn-thinhtran <ta642379@gmail.com>
2023-12-08 11:03:11 +0700

    update cors 4

--- src/main/java/com/qrcafe/config/CorsConfig.java
+++ src/main/java/com/qrcafe/config/CorsConfig.java
@@ -1,7 +1,5 @@
 package com.qrcafe.config;
 
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -14,20 +12,11 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        // corsConfiguration.setAllowedOriginPatterns(Collections.singletonList("*"));
-        // corsConfiguration.addAllowedHeader("*");
-        // corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOrigin("https://qr-order-client.netlify.app");       
+        corsConfiguration.addAllowedOrigin("https://qr-order-admin.netlify.app");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
         // corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(List.of("*"));
-        corsConfiguration.setAllowedMethods(List.of("HEAD",
-                "GET", "POST", "PUT", "DELETE", "PATCH"));
-        // setAllowCredentials(true) is important, otherwise:
-        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
-        corsConfiguration.setAllowCredentials(true);
-        // setAllowedHeaders is important! Without it, OPTIONS preflight request
-        // will fail with 403 Invalid CORS request
-        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
-        
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfiguration);
         
