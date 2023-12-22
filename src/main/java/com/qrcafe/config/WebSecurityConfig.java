3a29363b55b2f593295ed63ade551960391d4500
cmvn-thinhtran <ta642379@gmail.com>
2023-12-22 08:52:13 +0700

    login google test 1

--- src/main/java/com/qrcafe/config/WebSecurityConfig.java
+++ src/main/java/com/qrcafe/config/WebSecurityConfig.java
@@ -1,12 +1,14 @@
 package com.qrcafe.config;
 
+import com.qrcafe.oauth2.CustomAuthenticationSuccessHandler;
+import com.qrcafe.oauth2.CustomOAuth2UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,22 +17,21 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import jakarta.servlet.Filter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class WebSecurityConfig {
+
+    private final CustomOAuth2UserService customOauth2UserService;
+
+    private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
 
     private final JwtAuthEntryPoint authEntryPoint;
 
     @Autowired
     private JwtAuthenticationFilter jwtAuthenticationFilter;
-
-    @Autowired
-    public WebSecurityConfig(JwtAuthEntryPoint authEntryPoint) {
-        this.authEntryPoint = authEntryPoint;
-    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -46,6 +47,7 @@ public class WebSecurityConfig {
                 .and()
                 .authorizeRequests()
                 .requestMatchers("/api/auth/**").permitAll()
+                .requestMatchers("/auth/**", "/oauth2/**").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/product/**").permitAll()
                 .requestMatchers(HttpMethod.POST, "/api/product/**").hasAuthority("ADMIN")
                 .requestMatchers(HttpMethod.PUT, "/api/product/**").hasAuthority("ADMIN")
@@ -66,9 +68,14 @@ public class WebSecurityConfig {
                 .requestMatchers(HttpMethod.PUT, "/api/table/**").hasAuthority("ADMIN")
                 .requestMatchers(HttpMethod.DELETE, "/api/table/**").hasAuthority("ADMIN")
 
-
                 .and()
-                .httpBasic();
+                .httpBasic()
+                .and()
+                .oauth2Login(oauth2Login -> oauth2Login
+                        .userInfoEndpoint().userService(customOauth2UserService)
+                        .and()
+                        .successHandler(customAuthenticationSuccessHandler));
+
 
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
@@ -83,7 +90,7 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    PasswordEncoder passwordEncoder() {
+    static PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
--- src/main/java/com/qrcafe/service/impl/CustomUserDetailsService.java
+++ src/main/java/com/qrcafe/service/impl/CustomUserDetailsService.java
@@ -1,6 +1,7 @@
 package com.qrcafe.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -9,6 +10,10 @@ import org.springframework.stereotype.Service;
 import com.qrcafe.entity.CustomUserDetails;
 import com.qrcafe.entity.User;
 import com.qrcafe.repository.UserRepository;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -24,6 +29,21 @@ public class CustomUserDetailsService implements UserDetailsService {
         if (user == null) {
             throw new UsernameNotFoundException(username);
         }
-        return new CustomUserDetails(user);
+        List<SimpleGrantedAuthority> authorities = user.getRoles()
+                .stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName().toString()))
+                .collect(Collectors.toList());
+        return mapUserToCustomUserDetails(user, authorities);
+    }
+
+    private CustomUserDetails mapUserToCustomUserDetails(User user, List<SimpleGrantedAuthority> authorities) {
+        CustomUserDetails customUserDetails = new CustomUserDetails();
+        customUserDetails.setId(user.getId());
+        customUserDetails.setUsername(user.getUsername());
+        customUserDetails.setPassword(user.getPassword());
+        customUserDetails.setFullName(user.getFullName());
+        customUserDetails.setEmail(user.getEmail());
+        customUserDetails.setAuthorities(authorities);
+        return customUserDetails;
     }
 }
